e220beb98da96f333a34a3ec5a67f87389345ad9
Nikhil Sharma <nikhilsvs@gmail.com>
2020-06-06 15:58:19 +0530

    Controlled Forms

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Card,CardImg,CardBody,CardText,CardTitle} from 'reactstrap';
-
+import {Link } from 'react-router-dom';
+import {Breadcrumb,BreadcrumbItem} from 'reactstrap';
  
    
     
-    function RenderComments({dish}){
+    function RenderComments({comments}){
         
-        if(dish!=null)
+        if(comments!=null)
         {
             const months = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
-            const ans = dish.comments.map((x)=>{
+            const ans = comments.map((x)=>{
                 var d = new Date(x.date)
                 return(
                     <ul className="list-unstyled">
@@ -52,13 +53,23 @@ import {Card,CardImg,CardBody,CardText,CardTitle} from 'reactstrap';
         if(props.dish!=null)
         return(
             <div class="container">
-            <div className="row">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                </div>
+                <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     <RenderDish dish = {props.dish}/>
                 </div>  
                 <div className="col-12 col-md-5">
                     <h4>Comments</h4>
-                    <RenderComments dish = {props.dish}/>;
+                    <RenderComments dish = {props.comments}/>;
                 </div>    
                 
             </div>
