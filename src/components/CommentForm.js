5cf0e2665285db426cc313297b126c2c7e3d8a2c
Nikhil Sharma <nikhilsvs@gmail.com>
2020-06-10 14:21:33 +0530

    Combining  Reducers

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,8 +1,5 @@
-import React , {Component} from 'react';
-import {Card,CardImg,CardBody,CardText,CardTitle,Button 
-    , Modal,ModalHeader,ModalBody,Row,Col,Label} from 'reactstrap';
-import {Link } from 'react-router-dom';
-import {Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import React , {Component } from 'react';
+import {Button , Modal,ModalHeader,ModalBody,Row,Col,Label} from 'reactstrap';
 import {Control , LocalForm , Errors} from 'react-redux-form';
 
 const required = (val)=> val && val.length;
@@ -102,85 +99,5 @@ class CommentForm extends Component{
 
 
 }
- 
-   
-    
-    function RenderComments({comments}){
-        
-        if(comments!=null)
-        {
-            const months = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"];
-            const ans = comments.map((x)=>{
-                var d = new Date(x.date)
-                return(
-                    <ul className="list-unstyled">
-                        <li className="m-1">{x.comment}</li>
-                        <li className="m-1">--{x.author} , {months[d.getMonth()]} {d.getDate()} , {d.getFullYear()}</li>
-                    </ul>
-                );
-            });
-            return ans;
-        }
-        else{
-            return (
-                <div></div>
-            );
-        }
-         
-    }
 
-    function RenderDish({dish}){
-        if(dish!=null){
-            return(
-
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
-    }
-    const DishDetail = (props) => {
-        console.log("DishDetail Component render is Invoked")
-        if(props.dish!=null)
-        return(
-            <div class="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr/>
-                    </div>
-                </div>
-                <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish = {props.dish}/>
-                </div>  
-                <div className="col-12 col-md-5">
-                    <h4>Comments</h4>
-                    <RenderComments comments = {props.comments}/>;
-                    <CommentForm />
-                </div>    
-                
-                </div>
-                
-            </div>
-        );
-        else{
-            return(
-                <div></div>
-            );
-        }
-    }
-export default DishDetail;
+export default CommentForm;
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,6 +3,12 @@ import {Breadcrumb,BreadcrumbItem, Button, Label,Col,Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control,LocalForm ,Errors} from 'react-redux-form';
 
+const required = (val)=> val && val.length;
+const maxLength = (len) => (val)=> !(val) || (val.length<=len);
+const minlength = (len) => (val)=> (val) && (val.length>=len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component{
 
     constructor(props){
@@ -68,33 +74,73 @@ class Contact extends Component{
                     </div>
                     <div className="col-12 col-md-9">
                         <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
-                        <Row classNamw="form-group">
+                        <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name="firstname"
                                         placeholder="First Name"
                                         className="form-control"
+                                        validators={{
+                                            required,minLength:minlength(3),maxLength:maxLength(15)
+                                        }}
+                                         />
+                                         <Errors
+                                            className="text-danger"
+                                            model=".firstname"
+                                            show="touched"
+                                            messages={{
+                                                required:"Required",
+                                                minLength:"Must be Greater Than 2 Characters",
+                                                maxLength:"Must Be 15 Characters or Less"
+                                            }} 
                                          />
                                    
                                 </Col>
                             </Row>
-                            <Row classNamw="form-group">
+                            <Row className="form-group">
                                 <Label htmlFor="lastname" md={2}>Last Name</Label>
                                 <Col md={10}>
                                     <Control.text model=".lastname" id="lastname" name="lastname"
                                         placeholder="Last Name"
                                         className="form-control"
+                                        validators={{
+                                            required,minLength:minlength(3),maxLength:maxLength(15)
+                                        }}
+                                         />
+                                          <Errors
+                                            className="text-danger"
+                                            model=".lastname"
+                                            show="touched"
+                                            messages={{
+                                                required:"Required",
+                                                minLength:"Must be Greater Than 2 Characters",
+                                                maxLength:"Must Be 15 Characters or Less"
+                                            }} 
                                          />
                                  
                                 </Col>
                             </Row>
-                            <Row classNamw="form-group">
+                            <Row className="form-group">
                                 <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
                                 <Col md={10}>
                                     <Control.text model=".telnum" id="telnum" name="telnum"
                                         placeholder="Tel. Number"
                                         className="form-control"
+                                        validators={{
+                                            required,minLength:minlength(3),maxLength:maxLength(15), isNumber
+                                        }}
                                         />
+                                        <Errors
+                                            className="text-danger"
+                                            model=".telnum"
+                                            show="touched"
+                                            messages={{
+                                                required:"Required",
+                                                minLength:"Must be Greater Than 2 Characters",
+                                                maxLength:"Must Be 15 Characters or Less",
+                                                isNumber:"Tel Num Should Not conatin letters"
+                                            }} 
+                                         />
                                    
                                 </Col>
                             </Row>
@@ -108,7 +154,7 @@ class Contact extends Component{
                                    
                                 </Col>
                             </Row>
-                            <Row classNamw="form-group">
+                            <Row className="form-group">
                                <Col md={{size:6 , offset:2}}>
                                     <div className="form-check">
                                         <Label check>
@@ -128,7 +174,7 @@ class Contact extends Component{
                                    
                                </Col> 
                             </Row>
-                            <Row classNamw="form-group">
+                            <Row className="form-group">
                                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                                 <Col md={10}>
                                     <Control.textarea model=".message" id="message" name="message" 
@@ -136,7 +182,7 @@ class Contact extends Component{
                                    /> 
                                 </Col>
                             </Row>
-                            <Row classNamw="form-group">
+                            <Row className="form-group">
                                 <Col md={{size:10,offset:2}}>
                                     <Button type="submit" color="primary" block>
                                         Send FeedBack
